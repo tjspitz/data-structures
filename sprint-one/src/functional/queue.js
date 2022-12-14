c4af390f8b523a9054cece9094cd1ff8aba27bf2
Tom Spitz <tjspitz1986@gmail.com>
2022-12-14 16:56:36 -0600

    Implement 'The functional pattern' fully w/ tests passed.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,44 @@
+// No arrays! Instead, use an object with numeric keys
+
 var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
+  var getKeys = function() {
+    return Object.keys(storage);
+  };
 
+  // Implement the methods below
   someInstance.enqueue = function(value) {
+    // put the val 'on the back'...
+
+    // declare var to hold 'old' vals in an arr
+    // assign key 0 to hold 'value'
+    // for each el in the Obj.Vals arr
+      // assign storage @ [cur idx + 1] to equal cur val
+    var oldStorage = Object.values(storage);
+    var updater = function(val, idx) {
+      storage[idx + 1] = val;
+    };
+
+    storage['0'] = value;
+    oldStorage.forEach(updater);
+
   };
 
   someInstance.dequeue = function() {
+    // remove a val 'from the front'
+    // return that val
+    var dqed = storage[getKeys().length - 1];
+    delete storage[getKeys().length - 1];
+    return dqed;
   };
 
   someInstance.size = function() {
+    return getKeys().length;
+
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,46 @@
+// No arrays! Instead, use an object with numeric keys
+// USE YOUR WHITEBOARD TOM
+
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    // so abstract... are we looking to put anything in here? NOOOOOOO the user does not like garbage
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    // WILL get populated with methods and other 'user stuff'
+  };
+
+  var getKeys = function() {
+    return Object.keys(storage);
+  };
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // create a new prop with key '1 more than what's in storage' and val 'value'
+    // storage[the length of the keys arr] = value
+
+    storage[getKeys().length] = value;
+
   };
 
   someInstance.pop = function() {
+    // delete the prop w/ the highest-num key (since the keys are numeric)
+
+    // delete storage[the length of the keys arr MINUS 1]
+    // give me the thing I just deleted
+
+    var popped = storage[getKeys().length - 1];
+    delete storage[getKeys().length - 1];
+    return popped;
+
   };
 
   someInstance.size = function() {
+    return getKeys().length;
   };
 
   return someInstance;
 };
+
+// object.keys, .vals, return --> array, which has .length
