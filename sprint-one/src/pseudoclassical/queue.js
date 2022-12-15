f66dedca9da87d3137ac62f4f334adfaa3795244
Tom Spitz <tjspitz1986@gmail.com>
2022-12-14 22:42:40 -0600

    Implement 'The pseudoclassical pattern' fully w/ tests passed.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Queue.prototype.enqueue = function(value) {
+  var storage = this.storage;
+  var vals  = Object.values(storage);
+
+  this.storage['0'] = value;
+  _.each(vals, function(el, idx) {
+    storage[idx + 1] = el;
+  });
+}
+
+Queue.prototype.dequeue = function() {
+  var storage = this.storage;
+  var dqed = storage[Object.keys(storage).length - 1];
+
+  delete storage[Object.keys(storage).length - 1];
+  return dqed;
+}
+
+Queue.prototype.size = function() {
+  var storage = this.storage;
+
+  return Object.keys(storage).length;
+}
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  var storage = this.storage;
+  var keys = Object.keys(storage);
 
+  storage[keys.length] = value;
+}
+
+Stack.prototype.pop = function() {
+  var storage = this.storage;
+  var keys = Object.keys(storage);
+  var popped = storage[keys.length - 1];
+
+  delete storage[keys.length - 1];
+  return popped;
+}
+
+Stack.prototype.size = function() {
+  var storage = this.storage;
+
+  return Object.keys(storage).length;
+}
