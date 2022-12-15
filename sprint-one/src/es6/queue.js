cf6974500957366159396fa728620c7be0c1378b
Tom Spitz <tjspitz1986@gmail.com>
2022-12-14 23:01:01 -0600

    Implement 'The es6 pattern' fully w/ tests passed.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    var storage = this.storage;
+    var vals = Object.values(storage);
+
+    storage['0'] = value;
+    _.each(vals, function(el, idx) {
+      storage[idx + 1] = el;
+    });
+  }
+
+  dequeue() {
+    var storage = this.storage;
+    var dqed = storage[Object.keys(storage).length - 1];
+
+    delete storage[Object.keys(storage).length - 1];
+    return dqed;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    var keys = Object.keys(this.storage);
+
+    this.storage[keys.length] = value;
+  }
+
+  pop() {
+    var keys = Object.keys(this.storage);
+    var popped = this.storage[keys.length - 1];
+
+    delete this.storage[keys.length - 1];
+    return popped;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
