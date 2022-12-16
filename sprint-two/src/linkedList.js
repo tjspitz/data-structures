79f9533c7ba353d00176e717bb57e316d326aa5e
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-15 16:46:52 -0800

    Add first beginnings of linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,7 +4,26 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    // if head is null
+    if (list.head === null) {
+      // should point to invoking Node and passing in new value
+      list.head = newNode;
+      // tail should point to newly created Node
+      list.tail = list.head;
+      // if head is not null
+    } else {
+      // current node = list.head
+      // while current node doesn't equal null
+        // current node = newNode.next
+      // current node.next = newNode
+    }
   };
+
+      // if head is null,
+      // then list.head = new obj with two props {aVal:x, poinTo: {Node(newVal)}}
+    // otherwise
+      // {head: {aVal:x, poinTo: {next obj}}} lis.head.poinTo
 
   list.removeHead = function() {
   };
@@ -27,3 +46,6 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var newLlist = LinkedList();
+console.log(newLlist);
