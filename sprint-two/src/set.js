47a6e7fa03d87e147cb96486f87b655b59923bf6
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-17 11:39:31 -0800

    Add completed version of Set, passing all tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,37 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage[item]) {
+    this._storage[item] = 'true';
+  }
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+/*
+IS A SET
+[ 1, 2, 3, 4]
+
+
+IS NOT A SET
+[1, 1, 1, 2, 3, 4]
+
+
+*/
