dc7ee421e134f33b23e8fbc548e1db9e5d556dab
Tom Spitz <tjspitz1986@gmail.com>
2023-01-01 12:25:11 -0600

    Implemented basic methods (finally).

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,48 +8,67 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  // this.storage is where my key-vals are... LimitedArray makes the storage []
-  // is there anything in this.storage @ INDEX (var above)? if NOT, make a new bucket ( [] ) @ index
-  // this.storage --- push into it: k & v WITHIN their own ARRAY ---> so there aren't collisions!
-  // bucket @ 'index': [ [k, v] ]
-  console.log('what is THIS?: ', this);
-  console.log('what is index?: ', index);
-  console.log('what is storage?: ', this._storage);
-  console.log('is this a bucket?: ', this._storage[index]);
+  // is there anything in this.storage @ INDEX? if NOT, make a new bucket []
+  var bucket = this._storage.get(index) || [];
 
-  this._storage[index].push([k, v]);
+  // iterate over the 'bucket'
+    // if there is already a key for k
+    // (opt) store old value
+    // reassign value at k to v (new value)
+    // (opt) return old value
+
+  for (var i = 0; i < bucket.length; i ++) {
+    if (bucket[i][0] === k) {
+      var oldVal = bucket[i][1];
+      bucket[i][1] = v;
+      return oldVal;
+    }
+  }
+
+  // (no pre-existing keys for k found...)
+  // push into bucket: k & v WITHIN their own ARRAY ---> so there aren't collisions!
+  // use set to store index & val in limited array
+  // increment the size
+  bucket.push( [k, v] );
+  this._storage.set(index, bucket);
+  this._size ++;
+
+  // additional actions for checking size & resizing
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage[index];
-  for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) { // this._storage[1][i][0]
+  var bucket = this._storage.get(index) || [];
+
+  for (var i = 0; i < bucket.length; i ++) {
+    if (bucket[i][0] === k) {
       return bucket[i][1];
     }
   }
+  // return undefined if the arg doesn't exist in storage
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+
     // we have to iterate through k&v pairs in this.storage @ index
     // does this.storage @ index @ i @ 0 equal k? if so... // <--- not feeling 100% but close
       // remove this array (this.storage[index][i])
-  // var bucket = this._storage[index];
-  // return bucket.filter(function(item) {
-  //   return item[0] !== k;
-  // });
+
+    for (var i = 0; i < bucket.length; i ++) {
+      if (bucket[i][0] === k) {
+        bucket.splice(i, 1);
+        this._size --;
+        // additional actions for checking size & resizing
+      }
+
+    }
+  // return undefined if the arg doesn't exist in storage
+  return undefined;
+
 };
-
-//   THIS IS BUCKET -- remove k3 --- return an array = bucket w/ k3 filtered out
-// [
-// 0  [ k1, v],
-// 1  [ k2, v],
-// 2  [ k3, v],
-// 3  [ k4, v],
-// 4  [ k5, v]
-// ]
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
