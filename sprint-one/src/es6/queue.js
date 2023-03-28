4e4fe3c379013acbd7c3b2357ffd883c7e8d3894
Tom Spitz <tjspitz1986@gmail.com>
2023-03-28 14:47:04 -0500

    implement es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,27 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
-  }
 
+  constructor() {
+    this.storage = {};
+  }
+  enqueue(value) {
+    var storage = this.storage;
+    
+    for (var i = this.size(); i > 0; i--) {
+      storage[i] = storage[i - 1];
+    }
+    storage['0'] = value;
+  }
+  dequeue() {
+    var storage = this.storage;
+    var size = this.size();
+
+    var dequeued = storage[size - 1];
+    delete storage[size - 1];
+    return dequeued;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,17 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
-
+  push(value) {
+    this.storage[this.size()] = value;
+  }
+  pop() {
+    var popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return popped;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
