19e7beb1512ee683694620489ff8a0a16dc9e80d
Tom Spitz <tjspitz1986@gmail.com>
2023-03-29 15:06:05 -0500

    implement Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,60 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+  // _.extend(newTree, treeMethods);
+  Object.assign(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+  // push a new Tree into the caller's children array
+  // (every Tree is a Child / every Child is a Tree)
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+  // declare a boolean var to be updated
+  var found = false;
+  // if caller's value is target, return true
+  if (this.value === target) {
+    return true;
+  }
+  // for each child in this Tree's children array
+  this.children.forEach(function (child) {
+    // found is true from previous exec. context?
+    // return true
+    if (found) {
+      return true;
+    }
+    // update 'found' w/ call of .contains on current child
+    found = child.contains(target);
+  });
+
+  // return value of 'found'
+  return found;
+
+  /*
+    Note that .forEach is arguably *more* confusing than a for loop, in this application
+
+      if ( this.value === target ) {
+        return true;
+      }
+      for ( var i = 0; i < this.children.length; i++ ) {
+        var child = this.children[i];
+        if (child.contains(target)) {
+          return true;
+        }
+      }
+      return false;
+
+  */
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
