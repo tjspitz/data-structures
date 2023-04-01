e5477fc11680321d23665c85314665d1f8fec584
Tom Spitz <tjspitz1986@gmail.com>
2023-04-01 13:41:27 -0500

    implement Set, add a few Set tests

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -18,10 +18,35 @@ describe('set', function() {
     expect(set.contains('Susan Sarandon')).to.equal(true);
   });
 
+  it('should not duplicate values in a set', function() {
+    set.add('Bobby Bobberson');
+    set.add('Harry Harrelson');
+    set.add('Harry Harrelson');
+    set.add('Harry Harrelson');
+    expect(set.contains('Bobby Bobberson')).to.equal(true);
+    expect(set.contains('Harry Harrelson')).to.equal(true);
+    expect(Object.keys(set._storage)).to.have.lengthOf(2);
+  })
+
   it('should remove values from a set', function() {
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
     expect(set.contains('Mel Gibson')).to.equal(false);
+  });
+
+  it('should assign key-value pairs like a real JS Set', function() {
+    set.add('Peter Peterson');
+    set.add('Jojo Jojorson');
+    set.add('Ollie Ollerson');
+    var keys = Object.keys(set._storage);
+    var vals = Object.values(set._storage);
+    var expectation = true;
+    keys.forEach(function(key, idx) {
+      if (key !== vals[idx]) {
+        expectation = false;
+      }
+    });
+    expect(expectation).to.equal(true);
   });
 
 });
